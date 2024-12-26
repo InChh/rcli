903c0545c3054079a5f365d8051d056e6e3eac4f
InChh <2319397152@qq.com>
2024-12-26 17:29:05 +0800

    feature: add tower-http static file server

--- src/process/http.rs
+++ src/process/http.rs
@@ -1,11 +1,14 @@
+use axum::body::Body;
 use axum::extract::{Path, State};
-use axum::http::StatusCode;
+use axum::http::{HeaderMap, StatusCode};
+use axum::response::IntoResponse;
 use axum::routing::get;
-use axum::Router;
+use axum::{http, Router};
 use std::net::SocketAddr;
 use std::path::PathBuf;
 use std::sync::Arc;
 use tokio::net::TcpListener;
+use tower_http::services::ServeDir;
 use tracing::info;
 
 #[derive(Debug)]
@@ -16,10 +19,10 @@ pub struct AppState {
 pub async fn http_serve(dir: PathBuf, port: u16) -> anyhow::Result<()> {
     let address = SocketAddr::from(([0, 0, 0, 0], port));
     info!("Serving directory {:?} on {}", dir, address);
-
-    let state = AppState { path: dir };
+    let state = AppState { path: dir.clone() };
     let app = Router::new()
         .route("/*path", get(index_handler))
+        .nest_service("/tower-file-server", ServeDir::new(dir))
         .with_state(Arc::new(state));
     let listener = TcpListener::bind(address).await?;
     axum::serve(listener, app).await?;
@@ -29,19 +32,53 @@ pub async fn http_serve(dir: PathBuf, port: u16) -> anyhow::Result<()> {
 async fn index_handler(
     State(state): State<Arc<AppState>>,
     Path(path): Path<String>,
-) -> (StatusCode, String) {
+) -> impl IntoResponse {
+    let mut headers = HeaderMap::new();
     let file = state.path.join(&path);
     info!("Requesting file {:?}", file);
 
-    if !file.exists() || !file.is_file() {
-        return (StatusCode::NOT_FOUND, format!("File {:?} not found", file));
+    if !file.exists() {
+        return (
+            StatusCode::NOT_FOUND,
+            headers,
+            Body::from(format!("File {:?} not found", file)),
+        );
     }
 
-    match tokio::fs::read_to_string(file).await {
-        Ok(content) => (StatusCode::OK, content),
+    if file.is_dir() {
+        // If the path is a directory, we return a html page with the list of files
+        let mut content = String::new();
+        content.push_str("<html><body><ul>");
+        let mut entries = match tokio::fs::read_dir(&file).await {
+            Ok(entries) => entries,
+            Err(e) => {
+                return (
+                    StatusCode::INTERNAL_SERVER_ERROR,
+                    headers,
+                    Body::from(format!("Error reading directory {:?}: {:?}", file, e)),
+                );
+            }
+        };
+
+        while let Ok(Some(entry)) = entries.next_entry().await {
+            let path = entry.path();
+            let path = path.strip_prefix(&state.path).unwrap();
+            let name = entry.file_name();
+            let name = name.to_string_lossy();
+            content.push_str(&format!("<li><a href={:?}>{}</a></li>", path, name));
+        }
+        content.push_str("</ul></body></html>");
+        headers.insert(http::header::CONTENT_TYPE, "text/html".parse().unwrap());
+        return (StatusCode::OK, headers, Body::from(content));
+    }
+
+    headers.insert(http::header::CONTENT_TYPE, "text/plain".parse().unwrap());
+    match tokio::fs::read_to_string(&file).await {
+        Ok(content) => (StatusCode::OK, headers, Body::from(content)),
         Err(e) => (
             StatusCode::INTERNAL_SERVER_ERROR,
-            format!("Error reading file: {:?}", e),
+            headers,
+            Body::from(format!("Error reading file {:?}: {:?}", file, e)),
         ),
     }
 }
